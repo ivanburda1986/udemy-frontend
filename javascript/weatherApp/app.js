0a53c8b6e553cca4596acf5178ffff8f2c53a910
ivanburda1986 <burda.ivan@gmail.com>
2020-04-07 20:32:39 +0200

    finished building the weather app, however it does not work because the wunderground api has been discontinued for free access

---
+++ javascript/weatherApp/app.js
@@ -0,0 +1,40 @@
+//Init storage
+const storage = new Storage();
+
+//Get stored location data from the local storage
+const weatherLocation = storage.changeLocationData();
+
+//Initialize the weather class object
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+//Initialize the ui
+const ui = new UI();
+
+//Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  //Change location
+  weather.changeLocation(city, state);
+
+  //Set location in the local storage
+  storage.setLocationData(city, state);
+
+  //Get and display weather
+  getWeather();
+
+  //Close modal
+  $('#locModal').modal('hide');
+});
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      ui.paint(results);
+    })
+    .catch(err => console.log(err))
+}
