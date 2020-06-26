5fda75564799945fbbc7b57229e2ee6682351574
ivanburda1986 <burda.ivan@gmail.com>
2020-06-27 00:45:40 +0200

    started again with the trackalorie udemy exercise

--- javascript/calorie-tracker-project/app.js
+++ javascript/calorie-tracker-project/app.js
@@ -9,84 +9,57 @@ const ItemCtrl = (function () {
     this.calories = calories;
   }
 
-  //Data structure (State)
+  //Data structure / State
   const data = {
     items: [{
         id: 0,
-        name: 'Steak Dinner',
+        name: 'Steak dinner',
         calories: 1200
       },
       {
         id: 1,
-        name: 'Cookie',
+        name: 'Coookie',
         calories: 400
       },
       {
         id: 2,
         name: 'Eggs',
         calories: 300
-      },
+      }
     ],
     currentItem: null,
     totalCalories: 0
   }
 
-  //Publick methods
+
+  //Public methods
   return {
-    getItems: function () {
-      return data.items;
-    },
     logData: function () {
       return data;
     }
   }
-})();
 
+})();
 
 //UI controller
 const UICtrl = (function () {
-  const UISelectors = {
-    itemList: '#item-list'
-  }
 
-
-  //Publick methods
-  return {
-    populateItemList: function (items) {
-      let html = '';
-      items.forEach(function (item) {
-        html += `
-        <li class="collection-item" id="item-${item.id}">
-        <strong>${item.name}: </strong> <em>${item.calories} calories</em>
-        <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
-        </li>
-        `;
-      });
-
-      //Insert list items 
-      document.querySelector(UISelectors.itemList).innerHTML = html;
-    }
-  }
+  //Public methods
 
 })();
 
-
-
-//App controller - contains anything which should run when the app launches
+//App controller
 const App = (function (ItemCtrl, UICtrl) {
 
   //Public methods
   return {
     init: function () {
-      //Fetch items from data structure
-      const items = ItemCtrl.getItems();
-
-      //Populate list with items
-      UICtrl.populateItemList(items);
+      console.log('Initializing the app...');
     }
   }
 
 })(ItemCtrl, UICtrl);
 
-//Innitialize app
+
+//Initialize app
 App.init();
